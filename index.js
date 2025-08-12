8912f74fa2fe1f31b12e688d1e39e902ecc8287b
Chinasa <kamogelomba@gmail.com>
2025-08-12 16:00:51 +0200

    styling,javascript,uuid

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import mysql from 'mysql2/promise';
- 
+import { v4 as uuidv4 } from 'uuid'; 
 
 
 const app = express();
@@ -13,15 +13,15 @@ const pool = mysql.createPool({
   password:'justmondlisok'
 });
 
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"))
 
 
 
-
 let students
 
+
+      
  
 
 app.get("/", async (req,res)=>{
@@ -29,36 +29,34 @@ app.get("/", async (req,res)=>{
   try {
     const connection = await pool.getConnection();
 
-    const sql = 'SELECT *, DATE_FORMAT(DOB, "%Y-%m-%d") as DOB FROM students order by id ';
+    const sql = 'SELECT `DOB`,`name`,`email`,`phone`,`uuid`, DATE_FORMAT(DOB, "%Y-%m-%d") as DOB FROM students ';
     const [rows] = await connection.execute(sql);
-    students = rows;       
+    students = rows;
+     
+
     connection.release();
   } catch (error) {
     console.log(err) 
   }
-
   res.render("index.ejs",{
     students:students
   })
 })
 
 
-
-
-
 app.post("/newStudent", async (req,res)=>{
   
-  const studentName = req.body.name;
-  const studentEmail= req.body.email;
+  let uid = uuidv4().slice(0,3);
+
 
   try {
     const connection = await pool.getConnection()
     
-    const insert = 'INSERT INTO `students` (`name`, `email`)  VALUES ( ? , ?)';
-    const values = [studentName,studentEmail];
+    const insert = 'INSERT INTO `students` (`name`,`email`,`phone`,`DOB`, `uuid`) VALUES (?, ?, ?, ?,?)';
+    const values = [req.body.name ,req.body.email, req.body.phone, req.body.dob, uid];
 
     const [result, fields] = await connection.query(insert,values);
-
+    console.log(result)
   } catch (error) {
     console.log(error)
   }
@@ -94,36 +92,19 @@ app.post("/search", async (req,res)=>{
 })
 
 
-app.post("/show-all", async (req,res)=>{
-  try {
-    const connection = await pool.getConnection();
+app.post("/delete", async (req,res)=>{
+ const id = req.body.id
 
-    const sql = 'select * FROM students  ';
-    const [rows] = await connection.execute(sql);
-    students = rows;
-  } catch (error) {
-    console.log(err) 
-  }
-  res.redirect("/")
-})
 
-app.post("/delete/:id", async (req,res)=>{
-  const id = parseInt(req.params.id);
-  console.log(req.params.id)
 
   try {
    const connection = await pool.getConnection()
    console.log(id)
    
-   const sql = 'DELETE FROM `students` WHERE `id` = ? LIMIT 1';
+   const sql = 'DELETE FROM `students` WHERE `uuid` = ? LIMIT 1';
    const values = [id];
    const [result] = await connection.execute(sql,values);
-
-
-    const reply = 'select * FROM students  ';
-    const [rows] = await connection.execute(reply);
-
-    students = rows;
+   console.log(result);
 
   } catch (error) {
     console.log(error)
@@ -135,194 +116,33 @@ app.post("/delete/:id", async (req,res)=>{
 
 app.post("/editDetails", async (req,res)=>{
   const id = req.body.id;
-
-   console.log(`id is ${id}`);
    try {   
     const connection = await pool.getConnection()
-    let result = await 'select * from students where id = ?';
+    let result = await 'select * from students where uuid = ?';
     let values = [id];
     let [rows] = await connection.execute(result,values);
-    const oldRecord = rows[0]
-       console.log(oldRecord);
-       console.log(req.body)
+    let oldRecord = rows[0]
+       
 
-      const sql = 'UPDATE students SET `name` = ?, `email` = ? WHERE `id` = ? LIMIT 1';
-      const values1 = [
-        req.body.name || oldRecord.name,
-        req.body.email ||oldRecord.email,
-        req.body.phone ||oldRecord.phone,
-        req.body.dob ||oldRecord.DOB
-        ];
-        [result] = await connection.execute(sql, values1);
+       const sql = 'update students set name = ?, email = ?, phone = ?, DOB = ? WHERE uuid = ? LIMIT 1';
+       const values1 = [
+         req.body.name || oldRecord.name,
+         req.body.email ||oldRecord.email,
+         req.body.phone ||oldRecord.phone,
+         req.body.dob ||oldRecord.DOB,
+         [id]
+         ];
 
-
-
-
-    result = await 'select * from students where id = ?';
-    values = [id];
-    [rows] = await connection.execute(result,values);
-    const updatedRecord = rows[0]
-    console.log(updatedRecord)
-
-       } catch (error) {
+      let  [results,fields] = await connection.query(sql, values1)
+      
+      console.log(results);
+         
+        } catch (error) {
      console.log(error)
    }
 
-try {
-    const connection = await pool.getConnection();
-
-    const sql = 'select * FROM students order by id ';
-    const [rows] = await connection.execute(sql);
-    students = rows;       
-    connection.release();
-  } catch (error) {
-    console.log(err) 
-  }
-
   res.redirect("/")
 })                                                                    
-
-
-// Api code
-
-// Get all student records(id,name,email)
-app.get("/students",async (req,res)=>{
-
-  try {
-    const connection = await pool.getConnection();
-
-    const sql = 'select * FROM students  ';
-    const [rows] = await connection.execute(sql);
-
-    const students = rows;
-    res.json(students);
-    
-    connection.release();
-  } catch (error) {
-    console.log(err)
-  }
-})
-
-
-//Post new student(name,email)
-app.post("/newStudent", async (req,res)=>{
-
-  const studentName = req.body.name;
-  const studentEmail= req.body.email;
-
-  try {
-    const connection = await pool.getConnection()
-    
-
-    //insert into students(name,email) values ('John','john@email.com');
-    const insert = 'INSERT INTO `students` (`name`, `email`)  VALUES ( ? , ?)';
-    const values = [studentName,studentEmail];
-
-    const [result, fields] = await connection.query(insert,values);
-
-    const sql = 'select * FROM students  ';
-    const [rows] = await connection.execute(sql);
-
-    const newestAdd = rows[rows.length-1];
-    res.json(newestAdd);  
-    connection.release();
-  } catch (error) {
-    console.log(error)
-  } 
-})
-
-
-// Get specific student (id)
-app.get("/student/:id", async (req,res)=>{
-  const id = req.params.id;
-  try {
-    const connection = await pool.getConnection()
-    const result = await 'select * from students where id = ?';
-    const values = [id];
-    const [rows] = await connection.execute(result,values);
-    res.json(rows);
-    
-    connection.release();
-
-  } catch (error) {
-    console.log(error)
-  } 
-})
-
-
-//Patch student(email or name)
-app.patch("/updateStudent/:id", async (req,res)=>{
-  const id = req.params.id;
-  
-  
-
-  try {   
-   // get student to be edited
-    const connection = await pool.getConnection()
-    let result = await 'select * from students where id = ?';
-    let values = [id];
-    let [rows] = await connection.execute(result,values);
-    const oldRecord = rows[0]
-
-
-    // edit student 
-
-      const sql = 'UPDATE students SET `name` = ?, `email` = ? WHERE `id` = ? LIMIT 1';
-      const values1 = [
-        req.body.name || oldRecord.name,
-        req.body.email ||oldRecord.email,
-        id
-      ];
-
-    [result] = await connection.execute(sql, values1);
-    
-    
-    
-    //show edited record
-
-
-    result = await 'select * from students where id = ?';
-    values = [id];
-    [rows] = await connection.execute(result,values);
-    const updatedRecord = rows[0]
-
-    res.json(updatedRecord);
-
-    connection.release();
-  } catch (error) {
-    console.log(error)
-  } 
-})
-
-
-// Delete a student (id)
-
-app.delete("/delete/:id", async (req,res)=>{
-
-  try {
-   const connection = await pool.getConnection()
-   
-   const sql = 'DELETE FROM `students` WHERE `id` = ? LIMIT 1';
-   const values = [req.params.id];
-   const [result] = await connection.execute(sql,values)
-
-
-    // show all records
-
-
-    const reply = 'select * FROM students  ';
-    const [rows] = await connection.execute(reply);
-
-    const students = rows;
-    res.json(students);
-  connection.release();
-
-
-  } catch (error) {
-    console.log(error)
-  } 
-})
-
 
 
 app.listen(port, ()=>{
